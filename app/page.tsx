2b5d1fa4ab8678e571825b23029ff410bf646ee4
Avinash Tallapaneni <avinashtallapaneni@gmail.com>
2024-05-20 19:15:33 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "./login/page";
+
+export default function Home() {
+  return (
+    <main className="bg-[#0b132b] w-full h-screen grid place-items-center">
+      <LoginForm />
+    </main>
+  );
+}
